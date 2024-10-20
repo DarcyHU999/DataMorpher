94225055c409a3956b32ec9d7748a42df30b5285
DarcyHU999 <huqy1021_cd@outlook.com>
2024-10-20 17:53:01 +1100

    dockerization

--- DataMorpher/DataMorpher/settings.py
+++ DataMorpher/DataMorpher/settings.py
@@ -92,8 +92,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DataMorpher.wsgi.application'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
